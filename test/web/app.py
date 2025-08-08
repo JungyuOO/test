886a40ad12f184d3dc1d4a07a264128464cb405f
JungyuOO <rlawnsrb731@google.com>
2025-08-08 10:02:29 +0900

    챗봇 개발 진행중 1. 사용자 입력 완료 2. 이력 프린트 완료

--- test/web/app.py
+++ test/web/app.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
 st.title("챗봇이야")
+
+# streamlit에게 message_history 기억하게 하는 코드
+if "message_history" not in st.session_state:
+    st.session_state.message_history = []
+# 사용자의 입력 데이터
+# 사용자가 입력을 하지 않았다면 -> prompt = None
+prompt = st.chat_input("무엇이든지 물어봐 주세요")
+
+# prompt가 string이면서, 입력데이터가 존재할 때
+if isinstance(prompt, str) and prompt.strip(): # 사용자의 입력 데이터가 만약 있다면
+    st.session_state.message_history.append(
+        ("user", prompt) # 사용자가 입력하게 되면, message_history 에 추가 
+    )
+
+    
+for message in st.session_state.message_history: # 화면에 message_history 프린트
+    with st.chat_message(message[0]): # 사용자 아이콘
+        st.markdown(message[1]) # 사용자의 입력 메시지
